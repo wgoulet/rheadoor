2228a12acc7e72fa4cb2e671e13bbddab698d403
Walter Goulet <wgoulet@gmail.com>
2020-10-22 20:20:38 -0500

    Adding baseline stepper script using Adafruit DC MotorHat

---
+++ raspbi/adastepmotor.py
@@ -0,0 +1,14 @@
+import time
+import board
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+
+def main():
+    kit = MotorKit(i2c=board.I2C())
+    for i in range(20000):
+        kit.stepper1.onestep(direction=stepper.FORWARD,style=stepper.SINGLE)
+        time.sleep(0.001)
+
+
+if __name__ == "__main__":
+    main()
