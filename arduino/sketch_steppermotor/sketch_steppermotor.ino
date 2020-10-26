5e54ce465c4fbaa340b10596069cd17c38897e38
Walter Goulet <wgoulet@gmail.com>
2020-10-25 23:14:18 -0500

    Get basic I2C comms working but motor not moving smoothly

--- arduino/sketch_steppermotor/sketch_steppermotor.ino
+++ arduino/sketch_steppermotor/sketch_steppermotor.ino
@@ -3,12 +3,20 @@
 #define MS1 4
 #define MS2 5
 #define enablePin 6
+#define CMDMAX 25
+#include <Wire.h>
 
 bool hasRan = false;
-float numRotations = 0.25;
+float numRotations = 5;
+int cmdComplete = false;
+char cmd[CMDMAX];
+int cmdcount = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
-  
+  Wire.begin(0x8);
+  Wire.onReceive(receiveEvent);  
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(MS1, OUTPUT);
@@ -17,6 +25,36 @@ void setup() {
   resetEDPins(); //Set step, direction, microstep and enable pins to default states
   Serial.begin(9600); //Open Serial connection for debugging
   Serial.println("Begin motor control");
+}
+
+void receiveEvent(int numBytesReceived)
+{
+  Serial.println("Got an event");
+  
+  while(Wire.available())
+  {
+    char c = Wire.read();
+    Serial.println(int(c));
+    if(numBytesReceived < CMDMAX)
+    {
+      cmd[cmdcount] = int(c);
+      cmdcount++;
+    }
+  }
+  if(cmdcount == 2)
+  {
+    Serial.println("Got all data");
+    int motion = cmd[0];
+    int numRotation = cmd[1];
+    Serial.println(motion);
+    Serial.println(numRotation);
+    if(motion == 0x1) // 0x1 is forward
+    {
+      digitalWrite(enablePin,LOW);
+      moveForward(float(numRotation));
+    }
+    cmdcount = 0;
+  }
 }
 
 void resetEDPins()
@@ -50,6 +88,7 @@ void moveForward(float numRotations)
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1);
   while(Serial.available())
   {
     char userinput = Serial.read();
@@ -60,5 +99,6 @@ void loop() {
       resetEDPins();
     }
   }
+  
   
 }
